71dd6ce0bca6d6ce5025d64e66dfd10be5a0ff8b
mrodrig <rodrigues.mi@husky.neu.edu>
2018-10-02 01:34:59 +0000

    Adding support for nonexistent htpasswd files

--- test/manual/test.js
+++ test/manual/test.js
@@ -1,6 +1,7 @@
 let manager = require('../../src/manager.js');
-const filePath = './data/htpasswd';
+const filePath = './data/htpasswd',
+    nonExistentPath = './fakefile';
 
-let htpasswdManager = manager(filePath);
+let htpasswdManager = manager(nonExistentPath);
 
 htpasswdManager.addUser('user', 'pass123');
